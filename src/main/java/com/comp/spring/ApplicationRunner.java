f348ef9e36a95ac7e3417723f3c8d0a7fdb3b2b3
extranuts <valeev.timur.v@gmail.com>
2021-11-27 17:57:07 +0300

    FactoryMethod Intit

--- src/main/java/com/comp/spring/ApplicationRunner.java
+++ src/main/java/com/comp/spring/ApplicationRunner.java
@@ -2,29 +2,17 @@ package com.comp.spring;
 
 import com.comp.spring.database.pool.ConnectionPool;
 import com.comp.spring.database.repository.CompanyRepository;
-import com.comp.spring.database.repository.UserRepository;
-import com.comp.spring.ioc.Container;
-import com.comp.spring.service.UserService;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class ApplicationRunner {
 
     public static void main(String[] args) {
 
-        var container = new Container();
-
-//        var connectionPool = new ConnectionPool();
-//        var userRepository = new UserRepository(connectionPool);
-//        var companyRepository = new CompanyRepository(connectionPool);
-//        var userService = new UserService(userRepository, companyRepository);
-
-
-//        var connectionPool = container.get(ConnectionPool.class);
-//        var userRepository = container.get(UserRepository.class);
-//        var companyRepository = container.get(CompanyRepository.class);
-
-        var userService = container.get(UserService.class);
-        // TODO: 27.11.2021 userService
-
-
+        var context = new ClassPathXmlApplicationContext("application.xml");
+        // clazz ->  String-> Map<String, Object>
+        var connectionPool = context.getBean("p1", ConnectionPool.class);
+        System.out.println(connectionPool);
+        var companyRepository = context.getBean("companyRepository", CompanyRepository.class);
+        System.out.println(companyRepository);
     }
 }
