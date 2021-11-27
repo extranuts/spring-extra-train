ae83913b55295045cfb6b3849f15b210eb50b118
extranuts <valeev.timur.v@gmail.com>
2021-11-27 16:10:18 +0300

    Add IoC Container

--- src/main/java/com/comp/spring/ApplicationRunner.java
+++ src/main/java/com/comp/spring/ApplicationRunner.java
@@ -3,15 +3,28 @@ package com.comp.spring;
 import com.comp.spring.database.pool.ConnectionPool;
 import com.comp.spring.database.repository.CompanyRepository;
 import com.comp.spring.database.repository.UserRepository;
+import com.comp.spring.ioc.Container;
 import com.comp.spring.service.UserService;
 
 public class ApplicationRunner {
 
     public static void main(String[] args) {
-        var connectionPool = new ConnectionPool();
-        var userRepository = new UserRepository(connectionPool);
-        var companyRepository = new CompanyRepository(connectionPool);
-        var userService = new UserService(userRepository, companyRepository);
+
+        var container = new Container();
+
+//        var connectionPool = new ConnectionPool();
+//        var userRepository = new UserRepository(connectionPool);
+//        var companyRepository = new CompanyRepository(connectionPool);
+//        var userService = new UserService(userRepository, companyRepository);
+
+
+//        var connectionPool = container.get(ConnectionPool.class);
+//        var userRepository = container.get(UserRepository.class);
+//        var companyRepository = container.get(CompanyRepository.class);
+
+        var userService = container.get(UserService.class);
         // TODO: 27.11.2021 userService
+
+
     }
 }
