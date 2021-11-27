be459c83f5692af96604e1a4d9f94ac1181d65fe
extranuts <valeev.timur.v@gmail.com>
2021-11-27 18:08:16 +0300

    Setter Method Init

--- src/main/java/com/comp/spring/database/pool/ConnectionPool.java
+++ src/main/java/com/comp/spring/database/pool/ConnectionPool.java
@@ -4,10 +4,10 @@ import java.util.List;
 import java.util.Map;
 
 public class ConnectionPool {
-    private final String username;
-    private final Integer poolSize;
-    private final List<Object> args;
-    private final Map<String, Object> properties;
+    private  String username;
+    private  Integer poolSize;
+    private  List<Object> args;
+    private  Map<String, Object> properties;
 
     public ConnectionPool(String username,
                           Integer poolSize,
@@ -16,6 +16,10 @@ public class ConnectionPool {
         this.username = username;
         this.poolSize = poolSize;
         this.args = args;
+        this.properties = properties;
+    }
+
+    public void setProperties(Map<String, Object> properties) {
         this.properties = properties;
     }
 }
