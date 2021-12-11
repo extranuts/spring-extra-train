f9f749bdc853396bda719edf7ed55cd457f36b9a
extranuts <valeev.timur.v@gmail.com>
2021-12-11 17:34:36 +0300

    Custom Annotation BPP

--- src/main/java/com/comp/spring/database/repository/CompanyRepository.java
+++ src/main/java/com/comp/spring/database/repository/CompanyRepository.java
@@ -1,23 +1,36 @@
 package com.comp.spring.database.repository;
 
 import com.comp.spring.bpp.Auditing;
-import com.comp.spring.bpp.InjectBean;
 import com.comp.spring.bpp.Transaction;
 import com.comp.spring.database.entity.Company;
 import com.comp.spring.database.pool.ConnectionPool;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Repository;
 
 import javax.annotation.PostConstruct;
+import java.util.List;
 import java.util.Optional;
 
+@Repository
 @Transaction
 @Auditing
-public class CompanyRepository implements CrudRepository<Integer, Company>{
+public class CompanyRepository implements CrudRepository<Integer, Company> {
 
-    @InjectBean
-    private ConnectionPool connectionPool;
+    private final ConnectionPool pool1;
+    private final List<ConnectionPool> pools;
+    private final Integer poolSize;
+
+    public CompanyRepository(ConnectionPool pool1,
+                             List<ConnectionPool> pools,
+                             @Value("${db.pool.size}")Integer poolSize) {
+        this.pool1 = pool1;
+        this.pools = pools;
+        this.poolSize = poolSize;
+    }
 
     @PostConstruct
-    private void init(){
+    private void init() {
         System.out.println("init company repository");
     }
 
